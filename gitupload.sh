e9df199aa2115d4198f5e4309aa9d96593b35bcc
Benjamin Carpena <bencarpena@Helios.local>
2021-09-15 17:56:05 -0500

    20210915_175605

---
+++ gitupload.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# @bencarpena 20210202
+
+prefx = 'GitHub pages '
+dt=$(date '+%Y%m%d_%H%M%S');
+echo "=================================="
+echo "Commit tag: $dt"
+echo "=================================="
+
+git add .
+git commit -m $prex $dt
+git push origin main
